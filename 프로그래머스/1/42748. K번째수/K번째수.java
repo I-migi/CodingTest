c8ad636e6d1b0c4c4c0fd106cfc6e3235ebfbf75
이선우 <117967333+I-migi@users.noreply.github.com>
2024-06-05 23:13:04 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length;i++){
+            int[] newArray = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(newArray);
+            int value = newArray[commands[i][2]-1];
+            answer[i] = value;
+        }
+        
+
+        return answer;
+    }
+}
