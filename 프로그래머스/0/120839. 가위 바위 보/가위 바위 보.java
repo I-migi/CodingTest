c3452177088a66915d86ff705d9367434c8ba1fd
이선우 <117967333+I-migi@users.noreply.github.com>
2024-06-10 23:55:08 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(String rsp) {
+       
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<rsp.length();i++){
+            switch(rsp.charAt(i)){
+                case '2':
+                    sb.append('0');
+                    break;
+                case '0':
+                    sb.append('5');
+                    break;
+                case '5':
+                    sb.append('2');
+                    break;
+            }
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
