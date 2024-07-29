23a332a7c2a4fc531bd544a72231aead0b9ee1a3
이선우 <117967333+I-migi@users.noreply.github.com>
2024-07-29 20:25:29 +0900

    [level 2] Title: 타겟 넘버, Time: 9.45 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,6 @@
 class Solution {
-    int answer = 0; 
-
-    public int solution(int[] nums, int tgt) {
-        findWays(nums, 0, tgt, 0);
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
         return answer;
-    }
-
-    public void findWays(int[] nums, int idx, int tgt, int sum) {
-        if (idx == nums.length) {
-            if (sum == tgt) {
-                answer++;
-            }
-            return;
-        }
-        findWays(nums, idx + 1, tgt, sum + nums[idx]);
-        findWays(nums, idx + 1, tgt, sum - nums[idx]);
     }
 }
