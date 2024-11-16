72ffa3d8117ccf942a1b04f5036b3d981800465f
이선우 <117967333+I-migi@users.noreply.github.com>
2024-11-17 00:26:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 512 ms, Memory: 38504 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,5 +1,4 @@
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -7,41 +6,41 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static ArrayList<Integer>[] A;
 	static boolean visited[];
-	public static void main(String[] args) throws IOException {
+	static ArrayList<Integer>[] A;
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int V = sc.nextInt();
 
-		A = new ArrayList[N + 1];
+		A = new ArrayList[N+1];
+
 		for (int i =1; i<=N; i++) {
-			A[i] = new ArrayList<Integer>();
+			A[i] = new ArrayList<>();
 		}
 
-		for (int i =0; i<M; i++) {
-			int S = sc.nextInt();
-			int E = sc.nextInt();
-			A[S].add(E);
-			A[E].add(S);
+		for(int i = 1; i<=M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			A[a].add(b);
+			A[b].add(a);
 		}
 
-		for (int i =1; i<=N; i++){
+		for (int i = 1; i<=N; i++) {
 			Collections.sort(A[i]);
 		}
-		visited = new boolean[N + 1];
+
+		visited = new boolean[N+1];
 		DFS(V);
 		System.out.println();
-		visited = new boolean[N + 1];
+		visited = new boolean[N+1];
 		BFS(V);
-		System.out.println();
 	}
-
-	// DFS 는 재귀로 구하기
 	private static void DFS(int V) {
-		System.out.print(V + " ");
 		visited[V] = true;
+		System.out.print(V + " ");
 		for (int i : A[V]) {
 			if (!visited[i]) {
 				DFS(i);
@@ -49,15 +48,15 @@ public class Main {
 		}
 	}
 
-	// BFS 는 Queue 를 사용해서 구하기
 	private static void BFS(int V) {
-		Queue<Integer> queue = new LinkedList<Integer>();
+		Queue<Integer> queue = new LinkedList<>();
 		queue.add(V);
 		visited[V] = true;
 
 		while (!queue.isEmpty()) {
 			int currentV = queue.poll();
 			System.out.print(currentV + " ");
+
 			for (int i : A[currentV]) {
 				if (!visited[i]) {
 					visited[i] = true;
