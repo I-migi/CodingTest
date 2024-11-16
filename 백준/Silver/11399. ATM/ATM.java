1aece5e78eff478e6f0c879c12a859018af6122f
이선우 <117967333+I-migi@users.noreply.github.com>
2024-11-16 21:07:55 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,41 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] A = new int[N];
-		int[] S = new int[N];
-		for (int i = 0; i < N; i++) {
-			A[i] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+
+		for (int i =0; i< N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		for (int i =1; i< N; i++) {
-			int insert_point = i;
-			int insert_value = A[i];
-			for (int j = i-1; j>=0; j--){
-				if (A[j] < A[i]) {
-					insert_point = j+1;
-					break;
-				}
-				if (j == 0) {
-					insert_point = 0;
-				}
-			}
-			for (int j = i; j > insert_point; j--) {
-				A[j] = A[j-1];
-			}
-			A[insert_point] = insert_value;
-		}
+		Arrays.sort(arr);
 
-		S[0] = A[0];
+		int[] sumArr = new int[N];
+		sumArr[0] = arr[0];
 		for (int i =1; i<N; i++) {
-			S[i] = S[i-1] + A[i];
+			sumArr[i] = sumArr[i-1] + arr[i];
 		}
-
 		int sum = 0;
-		for (int i=0; i<N; i++) {
-			sum = sum + S[i];
+
+		for (int i =0; i<N; i++) {
+			sum = sum + sumArr[i];
 		}
 		System.out.println(sum);
 	}
