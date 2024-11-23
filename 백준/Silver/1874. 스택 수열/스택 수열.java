4e5aac09cf59abdc926a414048c7b868fd221b94
이선우 <117967333+I-migi@users.noreply.github.com>
2024-11-23 18:35:33 +0900

    [Silver II] Title: 스택 수열, Time: 344 ms, Memory: 31160 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,52 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] A = new int[N];
-		for (int i=0; i<N; i++) {
-			A[i] = sc.nextInt();
-		}
-		Stack<Integer> stack = new Stack<>();
+	public static void main(String[] args) throws IOException {
 
-		int num = 1;
-		boolean result = true;
-		StringBuffer bf = new StringBuffer();
-		for (int i=0; i<N; i++) {
-			int su = A[i];
-			if (su >= num) {
-				while (su >= num) {
-					stack.push(num++);
-					bf.append("+\n");
-				}
-				stack.pop();
-				bf.append("-\n");
-			} else {
-				int n = stack.pop();
-				if (n > su) {
-					System.out.println("NO");
-					result = false;
-					break;
-				}else {
-					bf.append("-\n");
-				}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder result = new StringBuilder();
+		int currentNumber = 1;
+
+		for (int i = 0; i < n; i++) {
+			int target = arr[i];
+
+			while (currentNumber <= target) {
+				stack.push(currentNumber);
+				result.append("+\n");
+				currentNumber++;
 			}
+
+			if (stack.peek() == target) {
+				stack.pop();
+				result.append("-\n");
+			} else {
+				bw.write("NO\n");
+				bw.flush();
+				return;
+			}
+
 		}
-		if (result) {
-			System.out.println(bf.toString());
-		}
+
+		bw.write(result.toString());
+		bw.flush();
+		bw.close();
 	}
 }
