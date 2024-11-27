bca7b8936af91b16d5f8f2029578a2533bc87c62
이선우 <117967333+I-migi@users.noreply.github.com>
2024-11-27 20:13:29 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,31 +2,30 @@
 import java.util.Scanner;
 
 public class Main {
-	static int answer = 0;
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String example = sc.nextLine();
-		String[] str = example.split("-");
+		Scanner scanner = new Scanner(System.in);
+		String problem = scanner.nextLine();
 
-		for (int i =0; i<str.length; i++) {
-			int temp = mySum(str[i]);
-			if (i == 0)
-				answer = answer + temp;
-			else
-				answer = answer - temp;
+		String[] split = problem.split("-");
+
+		int first = addSplit(split[0]);
+
+		for (int i = 1; i < split.length; i++) {
+			int second = addSplit(split[i]);
+			first = first - second;
 		}
-		System.out.println(answer);
 
+		System.out.println(first);
 
 	}
 
-	private static int mySum(String a) {
-		int sum =0;
-		String temp[] = a.split("[+]");
-		// a.split 에서 + 를 인식 못하는 경우가 가끔 발생 -> [ ] 로 감싸줘야 한다
-		for (int i =0; i<temp.length; i++) {
-			sum = sum + Integer.parseInt(temp[i]);
+	private static int addSplit(String problem) {
+		String[] split = problem.split("\\+");
+		int sum = 0;
+		for (int i = 0; i < split.length; i++) {
+			sum = sum + Integer.parseInt(split[i]);
 		}
 		return sum;
+
 	}
 }
