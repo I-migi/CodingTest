39988f6c311f89024c4ab241684c3596e47d3287
이선우 <117967333+I-migi@users.noreply.github.com>
2024-12-05 21:46:05 +0900

    [Gold II] Title: 버블 소트, Time: 1468 ms, Memory: 73820 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+
+
+		int[][] arr = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			arr[i][0] = Integer.parseInt(br.readLine());
+			arr[i][1] = i;
+		}
+
+		Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
+
+		int max = 0;
+
+		for (int i = 0; i < N; i++) {
+			max = Math.max(max, arr[i][1] - i);
+		}
+
+		System.out.println(max + 1);
+
+		
+	}
+}
